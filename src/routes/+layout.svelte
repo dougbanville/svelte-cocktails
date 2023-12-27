d7b937a5f05db8215b45e980d585e6453cda8611
Doug Banville <doug.banville@gmail.com>
2023-12-27 16:34:32 +0000

    Add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
-<nav>
-	<a href="/">home</a>
+<script>
+	import '../app.css';
+  </script>
+<nav class="p-4 bg-blue-500 text-white">
+	<a class="mr-4" href="/">home</a>
 	<a href="/cocktails">Cocktails</a>
 </nav>
 
--- src/routes/cocktails/[cocktail]/+page.svelte
+++ src/routes/cocktails/[cocktail]/+page.svelte
@@ -1,8 +1,20 @@
 <script>
     export let data;
     let cocktail = data.cocktail;
+    let ingredients = [];
+    for (let i = 1; i <= 15; i++) {
+        if (cocktail[`strIngredient${i}`]) {
+            ingredients.push(cocktail[`strIngredient${i}`]);
+        }
+    }
 </script>
 <main>
     <h1>{cocktail.strDrink}</h1>
+    <p>{cocktail.strInstructions}</p>
+    <ul>
+        {#each ingredients as ingredient}
+        <li>{ingredient}</li>
+        {/each}
+    </ul>
     <img src="{cocktail.strDrinkThumb}" alt="" srcset="">
 </main>
